9f839502d0193a5bce8cb94618af5de7cbea38eb
Jonathan Nevelson <jnevelson@gmail.com>
2013-05-24 23:44:51 -0700

    elevation gain

--- gpx.rb
+++ gpx.rb
@@ -1,14 +1,26 @@
 require 'nokogiri'
 
-locations = []
-Nokogiri::XML.parse(File.open(ARGV[0])).xpath("//xmlns:trkpt").each do |ele|
-  dimensions = {
-    :lat => ele.xpath('@lat')[0].content,
-    :lon => ele.xpath('@lon')[0].content,
-    :elevation => ele.xpath('xmlns:ele')[0].content,
-    :time => ele.xpath('xmlns:time')[0].content
-  }
-  locations << dimensions
+def build
+  @points = []
+  Nokogiri::XML.parse(File.open(ARGV[0])).xpath("//xmlns:trkpt").each_with_index do |ele, idx|
+    dimensions = {
+      :lat => ele.xpath('@lat')[0].content,
+      :lon => ele.xpath('@lon')[0].content,
+      :elevation => ele.xpath('xmlns:ele')[0].content.to_i,
+      :time => ele.xpath('xmlns:time')[0].content
+    }
+    @points << dimensions
+  end
 end
 
-puts locations
+def elevation_gain
+  gain = 0
+  @points.each_cons(2) do |p1, p2|
+    d = p2[:elevation] - p1[:elevation]
+    gain += d if d > 0
+  end
+  gain
+end
+
+build
+puts elevation_gain
