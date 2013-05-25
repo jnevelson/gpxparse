d4ddcfcdc36e482c1c86b25b089b4c851dc34ca6
Jonathan Nevelson <jnevelson@gmail.com>
2013-05-25 11:01:28 -0700

    classify

--- gpx.rb
+++ gpx.rb
@@ -1,30 +1,33 @@
 require 'nokogiri'
 
-def build
-  @points = []
-  Nokogiri::XML.parse(File.open(ARGV[0])).xpath("//xmlns:trkpt").each_with_index do |ele, idx|
-    dimensions = {
-      :lat => ele.xpath('@lat')[0].content,
-      :lon => ele.xpath('@lon')[0].content,
-      :elevation => ele.xpath('xmlns:ele')[0].content.to_i,
-      :time => ele.xpath('xmlns:time')[0].content
-    }
-    @points << dimensions
+class GpxParser
+  attr_accessor :points
+
+  def initialize
+    @points = []
+    Nokogiri::XML.parse(File.open(ARGV[0])).xpath("//xmlns:trkpt").each_with_index do |ele, idx|
+      dimensions = {
+        :lat => ele.xpath('@lat')[0].content,
+        :lon => ele.xpath('@lon')[0].content,
+        :elevation => ele.xpath('xmlns:ele')[0].content.to_i,
+        :time => ele.xpath('xmlns:time')[0].content
+      }
+      @points << dimensions
+    end
+  end
+
+  def elevation_diff
+    gain = 0
+    loss = 0
+    @points.each_cons(2) do |p1, p2|
+      d = p2[:elevation] - p1[:elevation]
+      gain += d if d > 0
+      loss += d if d < 0
+    end
+    [gain, loss]
   end
 end
 
-def elevation_diff
-  gain = 0
-  loss = 0
-  @points.each_cons(2) do |p1, p2|
-    d = p2[:elevation] - p1[:elevation]
-    gain += d if d > 0
-    loss += d if d < 0
-  end
-  [gain, loss]
-end
-
-build
-gain, loss = elevation_diff
+gain, loss = GpxParser.new.elevation_diff
 puts "Elevation gain: #{gain}m"
 puts "Elevation loss: #{loss}m"
